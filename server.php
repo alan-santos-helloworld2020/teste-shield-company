d8e27e38d1a88efc0bac0e084039fe113be1ab7f
Alan Paulo dos santos <onloadtecnologia@gmail.com>
2022-07-07 09:13:58 -0300

    first commit

--- server.php
+++
@@ -1,8 +0,0 @@
-<?php
-// $dados = ["id"=>2, "name"=>"José", "email"=>"jose@teste.com"];
-// echo json_encode($dados,JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_UNICODE);
-
-
-$x = array("aaa","ttt","www","ttt","yyy","tttt");
-$y = array_count_values($x);
-echo $y["ttt"];
